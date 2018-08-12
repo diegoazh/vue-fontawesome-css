f2d0e43edb2745131d53601cb1d262ff14212d02
Diego Zapata <diego.zapata@fukuroo.io>
2018-08-12 14:42:28 -0300

    Preparamos el plugin para testearlo con jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+  ],
+  transform: {
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,vue}',
+    '!**/node_modules/**',
+  ],
+  coverageReporters: ['html', 'text-summary'],
+  snapshotSerializers: [
+    'jest-serializer-vue',
+  ],
+};
