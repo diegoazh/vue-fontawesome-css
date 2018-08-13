af5d8932f2527828d3b44ee2487a2dec3baa9d9d
Diego Zapata <diego.zapata@fukuroo.io>
2018-08-12 22:43:44 -0300

    Dejamos los imports comentados por el momento, probablemente pasemos de cdn a @fortawesome/fontawesome-free

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -7,6 +7,11 @@
 </template>
 
 <script>
+// import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css';
+// import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css';
+// import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css';
+// import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css';
+
 export default {
   name: 'Icon',
   props: {
